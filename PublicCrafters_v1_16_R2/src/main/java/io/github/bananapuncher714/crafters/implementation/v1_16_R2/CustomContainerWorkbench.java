b714830984e9388f1f25269b902a6076efac5511
BananaPuncher714 <banana@aaaaahhhhhhh.com>
2020-08-13 09:31:57 -0400

    Update to 1.16.2

---
+++ PublicCrafters_v1_16_R2/src/main/java/io/github/bananapuncher714/crafters/implementation/v1_16_R2/CustomContainerWorkbench.java
@@ -0,0 +1,219 @@
+package io.github.bananapuncher714.crafters.implementation.v1_16_R2;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_16_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftHumanEntity;
+import org.bukkit.craftbukkit.v1_16_R2.inventory.CraftInventoryCrafting;
+import org.bukkit.craftbukkit.v1_16_R2.inventory.CraftInventoryView;
+import org.bukkit.entity.HumanEntity;
+
+import io.github.bananapuncher714.crafters.PublicCrafters;
+import io.github.bananapuncher714.crafters.util.Utils;
+import net.minecraft.server.v1_16_R2.AutoRecipeStackManager;
+import net.minecraft.server.v1_16_R2.ContainerAccess;
+import net.minecraft.server.v1_16_R2.ContainerWorkbench;
+import net.minecraft.server.v1_16_R2.EntityHuman;
+import net.minecraft.server.v1_16_R2.IInventory;
+import net.minecraft.server.v1_16_R2.IRecipe;
+import net.minecraft.server.v1_16_R2.InventoryClickType;
+import net.minecraft.server.v1_16_R2.InventoryCraftResult;
+import net.minecraft.server.v1_16_R2.InventoryCrafting;
+import net.minecraft.server.v1_16_R2.ItemStack;
+import net.minecraft.server.v1_16_R2.NonNullList;
+import net.minecraft.server.v1_16_R2.Slot;
+import net.minecraft.server.v1_16_R2.SlotResult;
+import net.minecraft.server.v1_16_R2.World;
+
+/**
+ * @author BananaPuncher714
+ */
+public class CustomContainerWorkbench extends ContainerWorkbench {
+	protected InventoryCraftResult resultInventory;
+	protected CustomInventoryCrafting craftInventory;
+	protected ContainerAccess containerAccess;
+	protected World world;
+	protected HumanEntity viewer;
+	private List< Slot > theseSlots;
+	
+	public CustomContainerWorkbench( int id, HumanEntity player, Location blockLocation, CustomInventoryCrafting crafting, InventoryCraftResult result ) {
+		super( id, ( ( CraftHumanEntity ) player ).getHandle().inventory );
+		
+		try {
+			Field slots = this.getClass().getField( "slots" );
+			theseSlots = ( List< Slot > ) slots.get( this );
+			
+		} catch ( Exception exception ) {
+			try {
+				Field c = this.getClass().getField( "c" );
+				theseSlots = ( List< Slot > ) c.get( this );
+			} catch ( Exception anotherException ) {
+				anotherException.printStackTrace();
+			}
+		}
+		theseSlots.clear();
+		
+		try {
+			Field slots = this.getClass().getField( "items" );
+			( ( NonNullList< ItemStack > ) slots.get( this ) ).clear();
+		} catch ( Exception exception ) {
+			try {
+				Field c = this.getClass().getField( "b" );
+				( ( NonNullList< ItemStack > ) c.get( this ) ).clear();
+			} catch ( Exception anotherException ) {
+				anotherException.printStackTrace();
+			}
+		}
+		
+		containerAccess = ContainerAccess.a;
+		viewer = player;
+		resultInventory = result;
+		craftInventory = crafting;
+		
+		world = ( ( CraftWorld ) blockLocation.getWorld() ).getHandle();
+
+		a( new SlotResult( ( ( CraftHumanEntity ) player ).getHandle(), craftInventory, resultInventory, 0, 124, 35));
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				a( new Slot( craftInventory, j + i * 3, 30 + j * 18, 17 + i * 18 ) );
+			}
+		}
+		for ( int i = 0; i < 3; i++ ) {
+			for (int j = 0; j < 9; j++) {
+				a( new Slot( ( ( CraftHumanEntity ) player ).getHandle().inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18 ) );
+			}
+		}
+		for ( int i = 0; i < 9; i++ ) {
+			a( new Slot( ( ( CraftHumanEntity ) player ).getHandle().inventory, i, 8 + i * 18, 142 ) );
+		}
+		a( craftInventory );
+	}
+	
+	// Overrides 1.15.1
+	@Override
+	 public ItemStack shiftClick(EntityHuman entityhuman, int i) {
+        ItemStack itemstack = ItemStack.b;
+        Slot slot = (Slot)this.slots.get(i);
+        if (slot != null && slot.hasItem()) {
+            ItemStack itemstack1 = slot.getItem();
+            itemstack = itemstack1.cloneItemStack();
+            if (i == 0) {
+                this.containerAccess.a( (world, blockposition) -> { itemstack1.getItem().b(itemstack1, world, entityhuman ); } );
+                if (!this.a(itemstack1, 10, 46, true)) {
+                    return ItemStack.b;
+                }
+                slot.a(itemstack1, itemstack);
+            } else if (i >= 10 && i < 46 ? !this.a(itemstack1, 1, 10, false) && (i < 37 ? !this.a(itemstack1, 37, 46, false) : !this.a(itemstack1, 10, 37, false)) : !this.a(itemstack1, 10, 46, false)) {
+                return ItemStack.b;
+            }
+            if (itemstack1.isEmpty()) {
+                slot.set(ItemStack.b );
+            } else {
+                slot.d();
+            }
+            if (itemstack1.getCount() == itemstack.getCount()) {
+                return ItemStack.b;
+            }
+            ItemStack itemstack2 = slot.a(entityhuman, itemstack1);
+            if (i == 0) {
+                entityhuman.drop(itemstack2, false);
+            }
+        }
+        return itemstack;
+    }
+	
+	public void setInventoryCrafting( CustomInventoryCrafting crafting ) {
+		craftInventory = crafting;
+	}
+	
+	@Override
+	public boolean a( ItemStack itemstack, Slot slot ) {
+		return isNotResultSlot( slot );
+	}
+	
+	@Override
+	public ItemStack a( int i, int j, InventoryClickType inventoryclicktype, EntityHuman entityhuman ) {
+		craftInventory.selfContainer.setContainer( this );
+		return super.a( i, j, inventoryclicktype, entityhuman );
+	}
+	
+	@Override
+	public void a( IInventory iinventory ) {
+		// Crafting
+		a( windowId, world, ( ( CraftHumanEntity ) viewer ).getHandle(), craftInventory, resultInventory, this );
+	}
+	
+	public boolean isNotResultSlot( Slot slot ) {
+		return slot.inventory != resultInventory;
+	}
+	
+	/**
+	 * This might be for when the inventory closes?
+	 */
+	@Override
+	public void b( EntityHuman entity ) {
+		craftInventory.selfContainer.setContainer( this );
+		super.b( entity );
+		// Make sure the craft inventory stops watching this container
+		craftInventory.removeContainer( this );
+		
+		if ( !world.isClientSide && PublicCrafters.getInstance().isDropItem() ) {
+			a( entity, world, craftInventory );
+			craftInventory.update();
+		}
+	}
+	
+	@Override
+	public boolean canUse( EntityHuman entity ) {
+		if ( !checkReachable ) {
+			return true;
+		}
+		// Should make more efficient
+		return craftInventory.getLocation().getBlock().getType() == Utils.getWorkbenchMaterial();
+	}
+
+	@Override
+	public CraftInventoryView getBukkitView() {
+		return new CraftInventoryView( viewer, new CraftInventoryCrafting( craftInventory, resultInventory ), this );
+	}
+
+	protected HumanEntity getViewer() {
+		return viewer;
+	}
+
+	@Override
+	public void a( AutoRecipeStackManager manager ) {
+		craftInventory.a( manager );
+	}
+
+	@Override
+	public boolean a( IRecipe< ? super InventoryCrafting > irecipe ) {
+		return irecipe.a( craftInventory, ( ( CraftHumanEntity ) viewer ).getHandle().world );
+	}
+
+	@Override
+	public void e() {
+		craftInventory.clear();
+		resultInventory.clear();
+	}
+
+	// getResultSlotIndex
+	@Override
+	public int f() {
+		return 0;
+	}
+
+	// getGridWidth
+	@Override
+	public int g() {
+		return craftInventory.g();
+	}
+
+	// getGridHeight
+	@Override
+	public int h() {
+		return craftInventory.f();
+	}
+}
---
+++ PublicCrafters_v1_16_R2/src/main/java/io/github/bananapuncher714/crafters/implementation/v1_16_R2/CustomInventoryCrafting.java
@@ -0,0 +1,182 @@
+package io.github.bananapuncher714.crafters.implementation.v1_16_R2;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_16_R2.inventory.CraftItemStack;
+
+import com.google.common.collect.Sets;
+
+import io.github.bananapuncher714.crafters.display.CraftDisplay;
+import io.github.bananapuncher714.crafters.implementation.API.PublicCraftingInventory;
+import io.github.bananapuncher714.crafters.implementation.v1_16_R2.ContainerManager_v1_16_R2.SelfContainer;
+import net.minecraft.server.v1_16_R2.Container;
+import net.minecraft.server.v1_16_R2.ContainerUtil;
+import net.minecraft.server.v1_16_R2.InventoryCraftResult;
+import net.minecraft.server.v1_16_R2.InventoryCrafting;
+import net.minecraft.server.v1_16_R2.ItemStack;
+
+/**
+ * The important class, this is universal and what makes crafting tables public
+ * 
+ * @author BananaPuncher714
+ */
+public class CustomInventoryCrafting extends InventoryCrafting implements PublicCraftingInventory {
+	Set< Container > containers = Sets.newHashSet();
+	private List< ItemStack > items;
+	private UUID id;
+	private Location bloc;
+	private CraftDisplay display;
+	private ContainerManager_v1_16_R2 manager;
+	protected SelfContainer selfContainer;
+	
+	public CustomInventoryCrafting( Location workbenchLoc, ContainerManager_v1_16_R2 manager, SelfContainer container, int i, int j ) {
+		super( container, i, j );
+		id = UUID.randomUUID();
+		bloc = workbenchLoc;
+		selfContainer = container;
+		this.manager = manager;
+		setDefaults();
+		display = new CraftDisplay( this );
+	}
+	
+	private void setDefaults() {
+		try {
+			// Set items
+			Field field = InventoryCrafting.class.getDeclaredField( "items" );
+			field.setAccessible( true );
+			items = ( List< ItemStack > ) field.get( this );
+		} catch ( Exception e ) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void setItem( int index, ItemStack item ) {
+		// Instead of updating one container, update all the containers
+		// That are looking at the table, basically the viewers
+		
+		items.set( index, item );
+		for ( Container container : containers ) {
+			container.a( this );
+		}
+		// Update the armorstand grid
+		display.update();
+	}
+	
+	@Override
+	public ItemStack splitStack( int i, int j ) {
+		ItemStack itemstack = ContainerUtil.a( items, i, j );
+		if ( !itemstack.isEmpty() ) {
+			for ( Container container : containers ) {
+				container.a( this );
+			}
+		}
+		// Update the armorstand grid
+		display.update();
+		return itemstack;
+	}
+	
+	// This is to fetch a nice list of Bukkit ItemStacks from the list of NMS ItemStacks
+	@Override
+	public List< org.bukkit.inventory.ItemStack > getBukkitItems() {
+		List< org.bukkit.inventory.ItemStack > bukkitItems = new ArrayList< org.bukkit.inventory.ItemStack >();
+		for ( ItemStack item : items ) {
+			bukkitItems.add( CraftItemStack.asBukkitCopy( item ) );
+		}
+		return bukkitItems;
+	}
+
+	@Override
+	public org.bukkit.inventory.ItemStack getResult() {
+		if ( this.resultInventory != null ) {
+			return CraftItemStack.asBukkitCopy( resultInventory.getItem( 0 ) );
+		}
+		return null;
+	}
+	
+	protected void setItems( List< org.bukkit.inventory.ItemStack > items ) {
+		int index = 0;
+		for ( org.bukkit.inventory.ItemStack item : items ) {
+			this.items.set( index++, CraftItemStack.asNMSCopy( item ) );
+		}
+		
+		// Want to update the result without having to use a real player
+		if ( this.resultInventory instanceof InventoryCraftResult ) {
+			CustomContainerWorkbench container = new CustomContainerWorkbench( 0, manager.mockPlayer.getBukkitEntity(), bloc, this, ( InventoryCraftResult ) resultInventory );
+			
+			container.a( this );
+		}
+		
+		display.forceUpdate();
+	}
+	
+	// Add another viewer
+	protected void addContainer( Container container ) {
+		containers.add( container );
+	}
+	
+	// Remove a container that stopped viewing it
+	protected void removeContainer( Container container ) {
+		containers.remove( container );
+	}
+	
+	protected void setLocation( Location newLoc ) {
+		bloc = newLoc;
+	}
+	
+	@Override
+	public UUID getUUID() {
+		return id;
+	}
+	
+	@Override
+	public Location getLocation() {
+		return bloc;
+	}
+	
+	@Override
+	public CraftDisplay getCraftDisplay() {
+		return display;
+	}
+	
+	@Override
+	public PublicCraftingInventory move( Location location ) {
+		display.stop();
+		if ( manager.get( bloc ) == this ) {
+			manager.benches.remove( bloc );
+		}
+		bloc = location;
+		CustomInventoryCrafting whatsHere = manager.put( location, this );
+		display = new CraftDisplay( this );
+		display.update();
+		return whatsHere;
+	}
+	
+	@Override
+	public void remove() {
+		display.stop();
+		for ( ItemStack item : items ) {
+			org.bukkit.inventory.ItemStack is = CraftItemStack.asBukkitCopy( item );
+			if ( is.getType() != Material.AIR ) {
+				bloc.getWorld().dropItem( bloc.clone().add( .5, .9, .5 ), is );
+			}
+		}
+	}
+	
+	@Override
+	public void update() {
+		if ( !bloc.getBlock().getType().name().equalsIgnoreCase( "CRAFTING_TABLE" ) ) {
+			remove();
+			manager.benches.remove( bloc );
+		} else {
+			manager.put( bloc, this );
+			display.update();
+		}
+	}
+}
